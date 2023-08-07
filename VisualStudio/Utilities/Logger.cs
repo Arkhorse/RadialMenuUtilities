cd92e865bae34edcd991967444cf189dab937372
Arkhorse <theillusivemman@gmail.com>
2023-08-07 18:17:22 -0300

    add FatalError

--- VisualStudio/Utilities/Logger.cs
+++ VisualStudio/Utilities/Logger.cs
@@ -7,6 +7,8 @@ namespace RadialMenuUtilities
         public static void Log(string message, params object[] parameters)              => Melon<Main>.Logger.Msg($"{message}", parameters);
         public static void LogWarning(string message, params object[] parameters)       => Melon<Main>.Logger.Warning($"{message}", parameters);
         public static void LogError(string message, params object[] parameters)         => Melon<Main>.Logger.Error($"{message}", parameters);
+        public static void LogFatalError(string message)                                => Melon<Main>.Logger.Error($"{message}", Color.red);
+
         public static void LogSeperator(params object[] parameters)                     => Melon<Main>.Logger.Msg("==============================================================================", parameters);
         public static void LogStarter()                                                 => Melon<Main>.Logger.Msg($"Mod loaded with v{BuildInfo.Version}");
     }
